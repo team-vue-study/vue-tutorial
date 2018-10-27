d6aa9d2ed16ce0ba7612e6964923694a6ef3290d
babywkb <>
2018-10-27 15:57:21 +0900

    ディーラーがカードを引く

--- vue-tutorial9/index.js
+++ vue-tutorial9/index.js
@@ -1,13 +1,26 @@
-Vue.component('trump', {
+Vue.component('player-hands', {
   props: ['cards'],
-  template: '<div><li v-for="card in cards">{{card.number}}{{card.suit}}</li></div>'
+  template: '<div>プレイヤー<li v-for="card in cards">{{card.number}}{{card.suit}}</li></div>'
+})
+Vue.component('dealer-hands', {
+  props: ['cards'],
+  template: '<div>ディーラー<li v-for="card in cards">{{card.number}}{{card.suit}}</li></div>'
 })
 
 var app = new Vue({
   el: '#app',
   data: {
-    hand: [],
-    totalValue: 0,
+    message: 'ディラー「勝負しましょ」',
+    player: {
+      name: 'あなた',
+      totalValue: 0,
+      hand: new Array
+    },
+    dealer: {
+      name: 'ディーラー',
+      totalValue: 0,
+      hand: new Array
+    },
     deck: [
       {
         number: 1,
@@ -21,17 +34,35 @@ var app = new Vue({
       }
     ]
   },
+  mounted: function () {
+    this.dealerDraw();
+  },
   methods: {
-    draw: function () {
-      this
+    draw: function (user) {
+      user
         .hand
         .push(this.deck[Math.floor(Math.random() * this.deck.length)]);
-    },
-    count: function () {
-      for(var value of this.hand) { // オブジェクトの中のプロパティ名を取り出す。
-        this.totalValue += value.number;
+      for (var value of user.hand) { // オブジェクトの中のプロパティ名を取り出す。
+        user.totalValue += value.number;
       }
-    }
-
+    },
+    judge: function (user) {
+      if (user.totalValue > 21) {
+        if (user.name == 'ディーラー') {
+          this.message = 'はいディラーの負け'
+        } else {
+          this.message = 'はい' + user.name + 'の負け'
+        }
+      }
+    },
+    playerDraw: function () {
+      this.draw(this.player);
+      this.judge(this.player);
+    },
+    dealerDraw: function () {
+      this.draw(this.dealer);
+      this.judge(this.dealer);
+    },
+    battle: function () {}
   }
 })
